e8c35e814789bba23d43ef85294989da2b5cad8d
ale.fabbian <fabbian.alessandro@gmail.com>
2023-07-11 18:35:05 +0200

    effetto funzionante

--- app.js
+++ app.js
@@ -2,36 +2,32 @@
 const title = document.querySelector('h1');
 let originalTitle = title.innerText;
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let intervalOn;
-let intervalOff;
+let iterations = 0;
+
 
 
 title.addEventListener('mouseover', e => {
-intervalOn = setInterval(() => {
-  letterChanger(e.target)
+  const interval = setInterval(() => {
+  letterChanger(e.target);
+  if(iterations >= e.target.length) clearInterval(interval)
+  iterations += 1/5;
 }, 30);
 
-intervalOff = setTimeout(()=> {
-  clearInterval(intervalOn)   
-}, 1000)
 })
 
 function letterChanger(e) {
     let targetLetters = e.innerText.split('')
-    let targetLettersSwitch = targetLetters.map(letter => letters[Math.floor(Math.random()*26)])
+    let targetLettersSwitch = targetLetters.
+    map((letter, index) => {
+      if(index < iterations) {
+        return e.dataset.value[index];
+      }
+
+      return letters[Math.floor(Math.random()*26)]
+    })
     let newText = targetLettersSwitch.join('')
+
     e.innerText = newText;
   }
 
-function letterReverser(e) {
-  let targetLettersSwitch = targetLetters.map(letter => letters[Math.floor(Math.random()*26)])
-  // let newText = targetLettersSwitch.join('')
-  // e.innerText = newText;
-}
-
-  
-// title.addEventListener('mouseout', e => {
-//   clearInterval(intervalOn)   
-//   e.target.innerText = originalTitle;
-// })
 
